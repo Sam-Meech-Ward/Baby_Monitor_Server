a43d3c5c09fdc77359727d942dabab2452baed4f
Sam Meech-Ward <sam@meech-ward.com>
2018-07-22 14:36:41 -0700

    allow the ws url to be posted to the http server

--- httpServer.js
+++ httpServer.js
@@ -4,15 +4,21 @@ let videoURL = "ws://192.168.1.71:8000/live/monitor.flv";
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index', {videoURL});
 });
 
-app.post('/video/:url', (req, res) => {
-  videoURL = res.params.url;
-  res.json('index', {videoURL});
+app.post('/video-url', (req, res) => {
+  videoURL = req.body.wsURL;
+  res.send({videoURL});
 })
 
 app.use(express.static('public'));
