036c03c87d025f701ab00178dea81ef5487a9899
Sam Meech-Ward <sam@meech-ward.com>
2018-07-22 18:36:32 -0700

    Remove pi scripts
    They are now located at https://gist.github.com/meech-ward/83745ee8f5009642adfffc6f355742c7

--- scripts/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ffmpeg -i tcp://127.0.0.1:8181?listen -c:v copy -c:a aac -ar 44100 -ab 40000 -f flv rtmp://127.0.0.1:4000/live/monitor &
-sleep 2
-~/picam/picam --alsadev hw:1,0 --tcpout tcp://127.0.0.1:8181 &
-node app.js
